398ccc9c383bca26239907634c1eadf80ef0be0e
Tanav Malhotra <161088369+tanav-malhotra@users.noreply.github.com>
2024-10-16 07:48:51 -0400

    Update ubuntu.sh

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -5,7 +5,7 @@ unalias -a
 clear
 echo "Created by Tanav Malhotra, Thomas A. Edison Career & Technical Education High School, New York City, NY, USA"
 sleep 3
-echo "Ubuntu Linux Script v1.0.2"
+echo "Ubuntu Linux Script v1.0.3"
 sleep 1
 echo "Starting..."
 sleep 1
