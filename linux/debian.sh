5dd69f7e2afba3502a4112fe34507a3db6859750
Tanav Malhotra <tanavm2009@gmail.com>
2024-10-25 18:52:32 -0400

    updated for round 1

--- linux/debian.sh
+++ linux/debian.sh
@@ -79,6 +79,12 @@ log;log;
 sleep 1
 
 # Confirming with user
+read -p "Do you want to make all of the bash scripts in this directory executable? (Y/n): " $confirmation
+if [[ $confirmation == n* || $confirmation == N* ]]; then
+    log "Make sure you manually run \`chmod +x\` on any script you want to run."
+else
+    chmod +x *.sh
+fi
 read -p "Have all of the Forensics Questions been answered? (Y/n): " $confirmation
 if [[ $confirmation == n* || $confirmation == N* ]]; then
     log "Please complete these first and only then rerun the script."
@@ -88,13 +94,6 @@ read -p "Have you created the required admins.txt, users.txt, addusers.txt, & ad
 if [[ $confirmation == n* || $confirmation == N* ]]; then
     log "Please create these files first by using the information from the README file located on your desktop."
     exit 1
-fi
-
-read -p "Do you want to make all of the bash scripts in this directory executable? (Y/n): " $confirmation
-if [[ $confirmation == n* || $confirmation == N* ]]; then
-    log "Make sure you manually run \`chmod +x\` on any script you want to run."
-else
-    chmod +x *.sh
 fi
 
 # Installing nala
