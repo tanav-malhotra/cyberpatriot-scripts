9bc842f621bcbbdeaede009dfdb8df80d6acd751
Tanav Malhotra <tanavm2009@gmail.com>
2024-11-02 18:18:57 -0400

    updated for round 2

---
+++ windows/script.bat
@@ -0,0 +1,17 @@
+:: ================================================================================
+:: Authors: Tanav Malhotra, Bryan Lochan 
+:: License: GNU General Public License v3.0
+:: Copyright (c) 2024 Tanav Malhotra, Bryan Lochan 
+::
+:: DISCLAIMER:
+:: THE SCRIPT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+:: KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+:: WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+:: PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+:: DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+:: CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR
+:: IN CONNECTION WITH THE SCRIPT OR THE USE OR OTHER DEALINGS
+:: IN THE SCRIPT.
+:: ================================================================================
+
