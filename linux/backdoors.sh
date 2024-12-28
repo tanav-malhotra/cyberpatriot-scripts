2d959827b0f460f56a12ac9e16797f0b1ff815b9
Tanav Malhotra <tanavm2009@gmail.com>
2024-12-28 16:17:28 -0500

    update for semifinals

--- linux/backdoors.sh
+++ linux/backdoors.sh
@@ -19,11 +19,6 @@
 
 ##### VARS #####
 LOGFILE="./backdoors_script.log"
-AIDE_CONF="/etc/aide/aide.conf"
-AIDE_DB="/var/lib/aide/aide.db"
-AIDE_DB_NEW="/var/lib/aide/aide.db.new"
-AIDE_BIN="/usr/bin/aide"
-AIDE_CONF_DIR="/etc/aide"
 
 ##### CHECK FOR SUDO #####
 if [[ $EUID -ne 0 ]]; then
@@ -39,8 +34,7 @@ line_sep() {
 # unusual or suspicious processes
 check_processes() {
     echo "Checking for suspicious processes (\`ps aux\`)..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     ps aux | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -49,40 +43,36 @@ check_ports() {
     echo "Installing netstat..." | tee -a $LOGFILE
     apt install -y net-tools
     echo "Checking for open ports (\`netstat -tulnp\`)..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
-    netstat -tulnp | tee -a $LOGFILE
+    log "Press 'Enter' to continue..."; read
+    netstat -tulnp | tee -a $LOGFILE | tee -a ./open_ports.log
     line_sep | tee -a $LOGFILE
 }
 # check cron jobs
 check_cron_jobs() {
     echo "Checking for suspicious cron jobs..." | tee -a $LOGFILE
+
     echo "\`crontab -l -u root\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     crontab -l -u root | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
+
     echo "\`ls /etc/cron.d\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     ls /etc/cron.d | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
+
     echo "\`ls /etc/cron.hourly\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     ls /etc/cron.hourly | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
+
     echo "\`ls /etc/cron.daily\`..." | tee -a $LOGFILE
-    read
+    log "Press 'Enter' to continue..."; read
     ls /etc/cron.daily | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
+
     echo "\`ls /etc/cron.weekly\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     ls /etc/cron.weekly | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -90,8 +80,7 @@ check_cron_jobs() {
 check_recent_files() {
     echo "Checking for recently modified files..." | tee -a $LOGFILE
     echo "\`find / -type f -ctime -7\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     find / -type f -ctime -7 | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -99,41 +88,35 @@ check_recent_files() {
 check_ssh() {
     echo "Checking SSH configuration and logs..." | tee -a $LOGFILE
     echo "\`cat /etc/ssh/sshd_config\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /etc/ssh/sshd_config | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     echo "\`/var/log/auth.log | grep ssh\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /var/log/auth.log | grep ssh | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
+#TODO: check configs for other critical services supported by debian.sh script
 # check for unusual user accounts
 check_users() {
     echo "Checking for unusual user accounts..." | tee -a $LOGFILE
     echo "\`cat /etc/passwd\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /etc/passwd | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     echo "\`cat /etc/shadow\`..." | tee -a $LOGFILE
-    read
+    log "Press 'Enter' to continue..."; read
     cat /etc/shadow | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cut -d: -f1 /etc/passwd | while read user; do
-        if id -nG "$user" | grep -qwE 'sudo|wheel|sudoers|admin'; then
+        if id -nG "$user" | grep -qwE 'sudo|wheel|admin'; then
             ROLE="admin"
         else
             ROLE="user"
         fi
         echo "Groups for $ROLE $user:" | tee -a $LOGFILE
         groups $user | tee -a $LOGFILE
-        log "Press 'Enter' to continue..."
-        read
+        log "Press 'Enter' to continue..."; read
     done
     line_sep | tee -a $LOGFILE
 }
@@ -141,17 +124,15 @@ check_users() {
 check_sudoers() {
     echo "Checking sudoers configuration..." | tee -a $LOGFILE
     echo "\`visudo -c\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     visudo -c | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
 # check for hidden network connections
 check_network_connections() {
     echo "Checking for hidden network connections..." | tee -a $LOGFILE
-    echo "\`lsof -i\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    echo "\`lsof -i\`..." | tee -a $LOGFILE | tee -a ./hidden_network_connections.log
+    log "Press 'Enter' to continue..."; read
     lsof -i | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -161,8 +142,7 @@ check_rootkit() {
     echo "Installing chkrootkit..." | tee -a $LOGFILE
     apt install -y chkrootkit
     echo "\`chkrootkit\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     chkrootkit | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -170,47 +150,39 @@ check_rootkit() {
 check_kernel_modules() {
     echo "Checking for unusual kernel modules..." | tee -a $LOGFILE
     echo "\`lsmod\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
-    lsmod | tee -a $LOGFILE
+    log "Press 'Enter' to continue..."; read
+    lsmod | tee -a $LOGFILE | tee -a ./kernel_modules.log
     line_sep | tee -a $LOGFILE
 }
 # check for suspicious GRUB modifications
 check_grub() {
     echo "Checking for suspicious GRUB configurations..." | tee -a $LOGFILE
     echo "\`cat /etc/default/grub\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
-    cat /etc/default/grub | tee -a $LOGFILE
+    log "Press 'Enter' to continue..."; read
+    cat /etc/default/grub | tee -a $LOGFILE | tee -a ./default_grub_config.log
     line_sep | tee -a $LOGFILE
 }
 # check suspicious services
 check_services() {
     echo "Checking for suspicious services..." | tee -a $LOGFILE
     echo "\`systemctl list-units --type=service --state=running\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
-    systemctl list-units --type=service --state=running | tee -a $LOGFILE
+    log "Press 'Enter' to continue..."; read
+    systemctl list-units --type=service --state=running | tee -a $LOGFILE | tee -a ./running_services.log
     line_sep | tee -a $LOGFILE
 }
 # check system logs
 check_logs() {
     echo "Checking system logs..." | tee -a $LOGFILE
     echo "\`cat /var/log/auth.log\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /var/log/auth.log | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     echo "\`cat /var/log/syslog\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /var/log/syslog | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     echo "\`cat /var/log/daemon.log\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
+    log "Press 'Enter' to continue..."; read
     cat /var/log/daemon.log | tee -a $LOGFILE
     line_sep | tee -a $LOGFILE
 }
@@ -220,10 +192,10 @@ check_file_integrity() {
     echo "Installing AIDE..." | tee -a $LOGFILE
     apt install -y aide
     echo "Configuring AIDE for CyberPatriot..." | tee -a $LOGFILE
-    cat > $AIDE_CONF <<EOF
+    cat > /etc/aide/aide.conf <<EOF
 # AIDE configuration for CyberPatriot
-database = $AIDE_DB
-database_out = $AIDE_DB_NEW
+database = /var/lib/aide/aide.db
+database_out = /var/lib/aide/aide.db.new
 gzip_dbout = yes
 
 # Exclude some directories that may change frequently
@@ -233,7 +205,7 @@ exclude = /var/log
 exclude = /var/run
 exclude = /var/cache
 
-# Directories to be monitored
+# Directories to be monitored - add more as needed
 dir = /etc
 dir = /bin
 dir = /sbin
@@ -247,7 +219,7 @@ dir = /var/spool
 dir = /var/lib
 dir = /var/opt
 
-# Files to be monitored
+# Files to be monitored - add more as needed
 file = /etc/passwd
 file = /etc/shadow
 file = /etc/group
@@ -259,18 +231,11 @@ EOF
 
     echo "AIDE configuration complete." | tee -a $LOGFILE
     echo "Initializing AIDE database..." | tee -a $LOGFILE
-    $AIDE_BIN --init
-    if [ $? -eq 0 ]; then
-        mv $AIDE_DB_NEW $AIDE_DB
-        echo "AIDE database initialized successfully." | tee -a $LOGFILE
-    else
-        echo "error: Failed to initialize AIDE database. Please check logs." | tee -a $LOGFILE # TODO: fix this error showing up every time
-        exit 1
-    fi
+    aideinit
+    mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
     echo "\`aide --check\`..." | tee -a $LOGFILE
-    log "Press 'Enter' to continue..."
-    read
-    aide --check | tee -a $LOGFILE
+    log "Press 'Enter' to continue..."; read
+    aide --check | tee -a $LOGFILE | tee -a ./aide_check.log
     line_sep | tee -a $LOGFILE
 }
 
@@ -280,47 +245,33 @@ apt update
 echo "Searching for backdoors..." | tee -a $LOGFILE
 line_sep | tee -a $LOGFILE
 check_processes
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_ports
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_cron_jobs
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_recent_files
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_ssh
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_users
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_sudoers
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_network_connections
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_rootkit
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_kernel_modules
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_grub
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_services
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_logs
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 check_file_integrity
-log "Press 'Enter' to continue..."
-read
+log "Press 'Enter' to continue..."; read
 echo "Finished searching for backdoors..."
 echo "Log saved to: $LOGFILE"
 
